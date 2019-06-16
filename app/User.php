9bc8f8acc22d035854e2c1296b82b8aa70c19800
JVFayad <joao.victor.fa2014@gmail.com>
2019-06-16 16:20:40 -0300

    fix

--- app/User.php
+++ app/User.php
@@ -10,30 +10,37 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'login', 'email', 'password', 'subscriber',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    protected $dates = [
+        'deleted_at'
+    ];
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    # Retorna todos os Posts do usuario
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
+
+    # Retorna todos os Comentarios do usuario
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    # Retorna todos os Comentarios do usuario
+    public function transactions()
+    {
+        return $this->hasMany('App\Transaction');
+    }
 }
