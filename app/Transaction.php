f5cea02aad6867e87eb70081ab6e6006e99288c7
JVFayad <joao.victor.fa2014@gmail.com>
2019-06-16 16:41:32 -0300

    model transaction

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'value', 'user_id'
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    # Retorna o Usuario que realizou a transacao
+    function user() {
+        return $this->belongsTo('App\User');
+    }
 }
