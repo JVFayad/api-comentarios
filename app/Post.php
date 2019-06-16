be6215ced8c10d58ff31effa745b45bcd2fc7c6c
JVFayad <joao.victor.fa2014@gmail.com>
2019-06-16 16:21:18 -0300

    post model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'content', 'type', 'user_id'
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    # Retorna o Usuario que fez a postagem
+    function user() {
+        return $this->belongsTo('App\User');
+    }
 }
