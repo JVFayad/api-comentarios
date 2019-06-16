793a57056890855b9e595f0ee52ba14c159bcb94
JVFayad <joao.victor.fa2014@gmail.com>
2019-06-16 16:27:04 -0300

    model comentarios

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'content', 'type', 'highlight', 'highlight_value', 'user_id', 'post_id'
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    # Retorna o Usuario que fez a Comentario
+    function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    # Retorna o Post referente a este Comentario
+    function post() {
+        return $this->belongsTo('App\Post');
+    }
 }
