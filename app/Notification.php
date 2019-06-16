a21602129d62fceaf571b68d33621785037c717e
JVFayad <joao.victor.fa2014@gmail.com>
2019-06-16 16:40:02 -0300

    model notification

--- app/Notification.php
+++ app/Notification.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    protected $fillable = [
+        'post_id', 'comment_id'
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    # Retorna o Post referente a notificacao
+    function post() {
+        return $this->belongsTo('App\Post');
+    }
+
+    # Retorna o Comentario referente a notificacao
+    function comment() {
+        return $this->belongsTo('App\Comment');
+    }
 }
